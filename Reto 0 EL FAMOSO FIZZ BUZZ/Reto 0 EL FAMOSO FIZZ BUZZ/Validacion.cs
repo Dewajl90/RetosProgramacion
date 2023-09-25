c0320d34d4634391a23f04089c02899cd2f95c15
Dewajl90 <145887010+Dewajl90@users.noreply.github.com>
2023-09-24 21:04:33 -0500

    FIZZ BUZZ
    Ingreso de archivos a la solución del reto 0

---
+++ Reto 0 EL FAMOSO FIZZ BUZZ/Reto 0 EL FAMOSO FIZZ BUZZ/Validacion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace Reto_0_EL_FAMOSO_FIZZ_BUZZ
+{
+    internal class Validacion
+    {
+        public int Number { get; set; }
+
+        public void PrintNumbers()
+        {
+            string palabra;
+            for (int i = 1; i < 101; i++)
+            {                
+                palabra = Multiplo(i);
+                if (palabra != "") 
+                {
+                    WriteLine($" {palabra}");
+                }else
+                    WriteLine($" {i}");
+            }
+        }//Imprime los números del 1 al 100
+
+        private string Multiplo(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                return "FIZZBUZZ";
+            }
+            else if (num % 3 == 0)
+            {
+                return "FIZZ";
+            }
+            else if (num % 5 == 0)
+            {
+                return "BUZZ";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
