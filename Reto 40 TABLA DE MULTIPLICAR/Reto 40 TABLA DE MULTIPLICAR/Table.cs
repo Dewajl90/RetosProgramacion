7bd59543a0e21af7d55f4c89e323b33e269334a7
Dewajl90 <145887010+Dewajl90@users.noreply.github.com>
2023-10-09 22:12:08 -0500

    Reto 40 Tabla De Multiplicar

---
+++ Reto 40 TABLA DE MULTIPLICAR/Reto 40 TABLA DE MULTIPLICAR/Table.cs
@@ -0,0 +1,24 @@
+﻿using static  System.Console;
+using static System.Convert;
+
+namespace Reto_40_TABLA_DE_MULTIPLICAR
+{
+    internal class Table
+    {
+        public int Number { get; set; }
+
+        public Table()
+        {
+            Write("Ingrese número:" );
+            this.Number = ToInt32(ReadLine());
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < 11; i++) 
+            {
+                WriteLine($"{this.Number} x {i} ={this.Number * i}");
+            }         
+        }
+    }
+}
